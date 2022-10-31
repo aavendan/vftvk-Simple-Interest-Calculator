143d80204ba79701f348c4999fba4b6a368f5a64
Allan <aavendan@gmail.com>
2022-10-30 23:54:22 -0500

    now

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
+function updateRate() 
+{
+    var rateval = document.getElementById("rate").value;
+    document.getElementById("rate_val").innerText=rateval;
+}
+
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+    var principal = document.getElementById("principal").value;
+    var rate = document.getElementById("rate").value;
+    var years = document.getElementById("years").value;
+
+    var interest = principal * years * rate / 100;
+
+    var amount = parseInt(interest);
+
+    var year = new Date().getFullYear()+parseInt(years);
+
+    document.getElementById("result").innerHTML="If you deposit "+principal+",\<br\>at an interest rate of "+rate+"%\<br\>You will receive an amount of "+amount+",\<br\>in the year "+year+"\<br\>"
 }
-        
+
+
+document.addEventListener('DOMContentLoaded', function() {
+   
+    document.getElementById("rate").addEventListener("change", function(){
+        updateRate()
+    })
+}, false);
